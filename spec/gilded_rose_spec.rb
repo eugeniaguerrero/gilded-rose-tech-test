a4fd624c28f8845738549b99533ccf1e7929fc2a
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-05-16 17:16:52 +0100

    adds tests for aged brie

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require 'gilded_rose'
 
 describe GildedRose do
 
@@ -6,8 +6,28 @@ describe GildedRose do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(items[0].name).to eq "foo"
     end
   end
+
+  context 'Aged Brie' do
+    it 'increases in quality' do
+      items = [Item.new("Aged Brie", 2, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 1
+    end
+    it 'decreases in sell in value after one day' do
+      items = [Item.new("Aged Brie", 2, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 1
+    end
+    it 'decreases in sell in value after two days' do
+      items = [Item.new("Aged Brie", 2, 0)]
+      GildedRose.new(items).update_quality()
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 0
+    end
+  end
+
 
 end
--- spec/gilded_rose_tests.rb
+++ spec/gilded_rose_tests.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require 'gilded_rose'
 require 'test/unit'
 
 class TestUntitled < Test::Unit::TestCase
@@ -6,7 +6,14 @@ class TestUntitled < Test::Unit::TestCase
   def test_foo
     items = [Item.new("foo", 0, 0)]
     GildedRose.new(items).update_quality()
-    assert_equal items[0].name, "fixme"
+    assert_equal items[0].name, "foo"
+  end
+
+  def test_normal_item_before_sell_date
+    items = [Item.new("normal", 10, 5)]
+    GildedRose.new(items).update_quality()
+    assert_equal 9, item.quality
+    assert_equal 5, item.sell_in
   end
 
 end
